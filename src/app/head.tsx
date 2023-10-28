391e9bbe144bc7de625939f0760403a643ffbfc5
Modou Aicha Diop <modouaichadiop@gmail.com>
2023-10-28 14:29:33 +0000

    init repo

---
+++ src/app/head.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Head() {
+  return (
+    <>
+    <title>Mikee.SN</title>
+      <meta charSet="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="description" content="A website for watch mangas animes, scans, manwha etc..." />
+    <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
