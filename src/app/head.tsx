4054ef5e5c9e125b2e8c8fcd07fb1c0509805d12
Modou Aicha Diop <modouaichadiop@gmail.com>
2023-11-06 10:53:06 +0000

    config projects done

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,13 +1,11 @@
-import React from 'react'
-
 export default function Head() {
   return (
     <>
     <title>Mikee.SN</title>
-      <meta charSet="UTF-8"/>
+    <meta charSet="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta name="description" content="A website for watch mangas animes, scans, manwha etc..." />
-    <link rel="icon" href="/favicon.ico" />
+    <link rel="icon" href='icon.jpg' />
     </>
   )
 }
