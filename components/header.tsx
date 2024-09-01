15ee524413f088a000ab3ff46c0a7bdb999ace0d
modouaicha023 <modouaichadiop@gmail.com>
2024-09-01 14:47:36 +0000

    add padding top to h1 container

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ interface HeaderProps {}
 export const Header: React.FC<HeaderProps> = ({}) => {
   return (
     <header className="flex flex-col ">
-      <div className="prose max-w-none w-full">
+      <div className="prose max-w-none w-full pt-4">
         <h1 className="text-center">Welcome⛩️</h1>
       </div>
       <Navbar />
